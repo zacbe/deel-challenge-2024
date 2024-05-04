6cf38b9f5719865762188f3878c73fcb421f88ee
@zacbe <zacbegonzalez@gmail.com>
2024-05-04 17:06:00 +0200

    Implement payment handler + payment transaction

--- src/controllers/jobs/pay.ts
+++ src/controllers/jobs/pay.ts
@@ -1,7 +1,17 @@
-
 import { Request, Response, NextFunction } from 'express';
+import { processJobPaymentById } from '../../services/jobService';
 
+export default async function handler(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const sequelize = req.app.get("sequelize");
+  const models = req.app.get("models");
+  const profileId = req.profile?.id
+  const jobId = req.params?.job_id;
 
-export default async function handler(_req: Request, res: Response, _next: NextFunction): Promise<void> {
-  res.json({ message: 'Pay' });
+  try {
+    await processJobPaymentById(jobId, profileId, { ...models, sequelize });
+    res.json({ message: "Successful Payment", });
+  } catch (e) {
+    next(e);
+  }
 }
+
--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -1,4 +1,4 @@
-import { Op } from 'sequelize';
+import { Op, Transaction } from 'sequelize';
 import { Job } from '../models/job';
 
 
@@ -21,4 +21,39 @@ async function findAllUnpaidJobsByProfile(profileId: number | undefined, models:
   });
 }
 
-export { findAllUnpaidJobsByProfile };
+async function processJobPaymentById(jobId: string, profileId: number | undefined, models: any): Promise<void> {
+  const { Job, sequelize } = models;
+
+  try {
+    await sequelize.transaction(async (t: typeof Transaction) => {
+      const job = await Job.findByPk(jobId);
+      if (!job) throw new Error("Job not found");
+      if (job.paid) throw new Error("Job has already been paid");
+
+      const contract = await job.getContract();
+      const client = await contract.getClient({ where: { id: profileId } });
+      if (!client) throw new Error("Client not found");
+      if (client.balance < job.price) throw new Error("Insufficient funds");
+
+      const contractor = await contract.getContractor();
+
+      client.balance -= job.price;
+      contractor.balance += job.price;
+      job.paid = true;
+      job.paymentDate = new Date();
+
+      await client.save({ transaction: t })
+      await contractor.save({ transaction: t })
+      await job.save({ transaction: t })
+
+    });
+
+    console.info("Transaction completed successfully");
+  } catch (error) {
+    console.error("Transaction failed:", error);
+    throw error;
+  }
+}
+
+
+export { findAllUnpaidJobsByProfile, processJobPaymentById };
