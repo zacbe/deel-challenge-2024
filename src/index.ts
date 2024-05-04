119edcf3ceade7f5f8c4d98bab73e53c78f112c1
@zacbe <zacbegonzalez@gmail.com>
2024-05-04 16:23:08 +0200

    Implement envalid + add all missing @types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
+import { env } from './utils/envConfig';
 import app from './server';
 
+const server = app.listen(env.PORT, () => {
+  const { NODE_ENV, HOST, PORT } = env;
+  console.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+});
 
-init();
+const onCloseSignal = () => {
+  console.info('sigint received, shutting down');
+  server.close(() => {
+    console.info('server closed');
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+};
 
-async function init() {
-  try {
-    app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
-    });
-  } catch (error) {
-    console.error(`An error occurred: ${JSON.stringify(error)}`);
-    process.exit(1);
-  }
-}
+process.on('SIGINT', onCloseSignal);
+process.on('SIGTERM', onCloseSignal);
