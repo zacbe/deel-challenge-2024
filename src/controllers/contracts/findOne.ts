f779a21d984973e8b25abc4f9095fc48fecbece5
@zacbe <zacbegonzalez@gmail.com>
2024-05-04 16:49:40 +0200

    Implement handler with contractService

--- src/controllers/contracts/findOne.ts
+++ src/controllers/contracts/findOne.ts
@@ -1,7 +1,20 @@
-
+import createError from "http-errors";
 import { Request, Response, NextFunction } from 'express';
+import { findContractById } from "../../services/contractService";
 
+export default async function handler(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const contractId = req.params?.id;
+  const profileId = req.profile?.id
+  const { Contract } = req.app.get("models");
 
-export default async function handler(_req: Request, res: Response, _next: NextFunction): Promise<void> {
-  res.json({ message: 'findOne' });
+  try {
+
+    const contract = await findContractById(contractId, profileId, Contract);
+    if (!contract) {
+      return next(createError(404, "Contract not found"));
+    }
+    res.json({ contract });
+  } catch (e) {
+    next(e);
+  }
 }
